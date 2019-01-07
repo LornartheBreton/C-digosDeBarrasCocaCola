7a5d8d6c678e636d8d7a7efe84414004cdb65ef7
Lornar the Breton <hectorspacejam@gmail.com>
2019-01-07 16:17:38 -0600

    Update systems.ts

--- src/pages/systems/systems.ts
+++ src/pages/systems/systems.ts
@@ -6,11 +6,9 @@ import { isRightSide } from 'ionic-angular/umd/util/util';
 import { HomePage } from '../home/home'
 import { TasksServiceProvider } from '../../providers/tasks-service/tasks-service';
 import { Storage } from "@ionic/storage";
-/**
- * Generated class for the SystemsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+/*
+  Éste archivo define el comportamiento de la página "systems": la primera que
+  aparece al iniciar la aplicación y de la que desembocan las demás páginas.
  */
 
 @IonicPage()
@@ -22,7 +20,7 @@ export class SystemsPage {
   system=0;
   home=HomePage;
   specs=SpecsPage;
-  items = [];
+  items = []; //Éste es el arreglo que contiene todos los equipos de cómputo.
   temp={
     CPU:"",
     RAM:"",
@@ -31,7 +29,7 @@ export class SystemsPage {
     URL:"",
     DEP: "",
     MAR: ""
-  };
+  };//Éste es el diccionario cuya estructura comparten todos los elementos del arreglo items.
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public dataService: DataProvider, public tasksService: TasksServiceProvider,
     public storage: Storage) {
@@ -65,18 +63,20 @@ export class SystemsPage {
         }else{
           this.storage.set('systems', JSON.stringify(this.items));
         }
-      });
+      });/*Al entrar en la aplicación se verifica si es la primera ves que se inicia. 
+      En el caso que no, se crea el archivo JSON en dónde se guardaran todos los elementos
+      de "items".*/
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SystemsPage');
   }
   seeSpecs(thing: any){
-    this.navCtrl.push(this.specs, thing)
+    this.navCtrl.push(this.specs, thing) //Pasa a una página para ver las especificaciones del equipo.
   }
 
   addSystem(){
-    this.navCtrl.push(this.home, {items: this.items});
+    this.navCtrl.push(this.home, {items: this.items});//Para ala pagina de "homa" para agregar un equipo.
   }
 
   /*getAllSystems(){
